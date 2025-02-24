32ed1223cbf8dc197debb133c1ce119651b2a6a0
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-02-24 11:19:06 +0100

    Disable minification to aid in debugging

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require L.Google/L.Google.js
 //= require leaflet-control-window/L.Control.Window.js
 //= require leaflet-fullscreen/Control.FullScreen.js
-//= require leaflet.tilelayer.wmts/leaflet-tilelayer-wmts.js
+//= require leaflet.tilelayer.wmts/leaflet-tilelayer-wmts-src.js
 //= require wellknown/wellknown.js
 //= require leaflet-label/leaflet.label.js
 //= require leaflet-geodesy/leaflet-geodesy.js
